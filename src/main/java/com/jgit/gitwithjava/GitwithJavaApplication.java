01b5c1e2ca01782047c00a9e8929d1755434238a
rishabh-ioanyt <rishabh.maurya@ioanyt.com>
2023-09-05 19:00:03 +0530

    github create repository

--- src/main/java/com/jgit/gitwithjava/GitwithJavaApplication.java
+++ src/main/java/com/jgit/gitwithjava/GitwithJavaApplication.java
@@ -20,7 +20,7 @@ public class GitwithJavaApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(GitwithJavaApplication.class, args);
         GitHubRestApiService gitHubRestApiService  = new GitHubRestApiService();
-        /*File file = new File(DefaultCredentials.getRootFolder()+"Videos/springBootProject/GitwithJava");
+       /* File file = new File(DefaultCredentials.getRootFolder()+"Videos/springBootProject/GitwithJava");
         Git git = Git.open(file);
         GitCustomService gitCustomService = new GitCustomService();
         gitCustomService.gitPush(git);
--- src/main/java/com/jgit/gitwithjava/frontend/controller/GithubController.java
+++ src/main/java/com/jgit/gitwithjava/frontend/controller/GithubController.java
@@ -1,10 +1,18 @@
 package com.jgit.gitwithjava.frontend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
 import com.jgit.gitwithjava.github.service.GitHubRestApiService;
+import org.apache.logging.log4j.util.Strings;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Objects;
 
 @Controller
 public class GithubController {
@@ -21,5 +29,11 @@ public class GithubController {
     @GetMapping("/githubForm")
     public String githubForm(){
         return "githubForm";
+    }
+
+    @PostMapping("/createGitHubRepo")
+    public String createGitHubRepo(Model model,Object repoModel) throws Exception {
+        //model.addAttribute("listAllRepository",gitHubRestApiService.listAllRepository());
+        return "githubRepo";
     }
 }
