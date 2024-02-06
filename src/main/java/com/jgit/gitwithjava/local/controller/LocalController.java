8f3bbd739633d4c7cf142251af2b495f6196f892
rishabh-ioanyt <rishabh.maurya@ioanyt.com>
2024-02-06 22:00:43 +0530

    fix new home

--- src/main/java/com/jgit/gitwithjava/local/controller/LocalController.java
+++ src/main/java/com/jgit/gitwithjava/local/controller/LocalController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("local")
@@ -29,5 +30,12 @@ public class LocalController {
     public String fileDetails(Model model, String path) {
         model.addAttribute("path", path);
         return "details";
+    }
+
+    @GetMapping("/createFolder")
+    public RedirectView createFolder(Model model, String path, String directoryName){
+        System.out.println(path);
+        System.out.println(directoryName);
+        return new RedirectView("local?path="+path);
     }
 }
